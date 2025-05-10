2a9daf37ec856ec832a2cfca0f675ff9f99e8eed
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-05-10 11:42:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+let form = document.getElementById("form");
+form.addEventListener("submit",(event)=>{
+	event.preventDefault();
+	let name=document.getElementById("name");
+	let year=document.getElementById("year");
+	let nameOnUrl = "";
+	let yearOnUrl = "";
+	if(name.value!=""){
+		nameOnUrl="?name="+name.value;
+	}
+	if(year.value!="" && name.value==""){
+		yearOnUrl="?year="+year.value;
+	}
+	else if(year.value!="" && name.value!=""){
+		yearOnUrl="&year="+year.value;
+	}
+	let result = nameOnUrl+yearOnUrl
+
+	let url = document.getElementById("url");
+	url.innerHTML = "https://localhost:8080/"+result;
+})
